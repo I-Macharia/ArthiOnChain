b5b007355f9b765db2672cdeeaa7fac010d6a60d
“I-Macharia” <gichoya.macharia@gmail.com>
2024-10-12 02:14:13 +0300

    created smart wallet script

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-# Initialize Coinbase Wallet SDK
+// Initialize Coinbase Wallet SDK
 const sdk = new CoinbaseWalletSDK({appName: "ArdhiOnchain", appChainIds: [84532]});
 
-# Make web3 provider
+//  Make web3 provider
 const provider = sdk.makeWeb3Provider();
 
-# Initialize wallet connection
+//  Initialize wallet connection
 const addresses = provider.request("eth_requestAccounts");
