0a47463e141394807a856386ccf41a02cdbf7178
I-Macharia <gichoya.macharia@gmail.com>
2024-10-13 02:22:00 +0300

    updating login contract with smart wallet

--- contracts/LoginContract.sol
+++ contracts/LoginContract.sol
@@ -1,28 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.23;
 
-contract Login {
+import "https://github.com/I-Macharia/smart-wallet/blob/main/src/CoinbaseSmartWallet.sol";
+import "https://github.com/I-Macharia/smart-wallet/blob/main/src/MultiOwnable.sol";
+import "https://github.com/I-Macharia/webauthn-sol/blob/main/src/WebAuthn.sol";
+import "./lib/solady/utils/LibString.sol";
+
+contract LoginContract {
+    // Mapping of user addresses to registration status
     mapping(address => bool) public isRegistered;
-    address public connectedWallet;
 
+    // Event emitted when a user registers
     event UserRegistered(address indexed userAddress);
 
-    constructor() {
-        connectedWallet = address(0); // Initialize connected wallet address to 0
-    }
-
-    function connectWallet(address _walletAddress) public {
-        connectedWallet = _walletAddress;
-    }
-
+    // Function to register a user
     function register() public {
-        require(!isRegistered[connectedWallet], "User  already registered");
-        isRegistered[connectedWallet] = true;
+        // Get the BASE smart wallet instance
+        BaseSmartWallet smartWallet = BaseSmartWallet(address(this));
 
-        emit UserRegistered(connectedWallet);
+        // Register the user using the BASE smart wallet
+        smartWallet.register(msg.sender);
+
+        // Emit the UserRegistered event
+        emit UserRegistered(msg.sender);
+
+        // Set the user's registration status to true
+        isRegistered[msg.sender] = true;
     }
 
+    // Function to check if a user is registered
     function isUserRegistered() public view returns (bool) {
-        return isRegistered[connectedWallet];
+        // Get the BASE smart wallet instance
+        BaseSmartWallet smartWallet = BaseSmartWallet(address(this));
+
+        // Check if the user is registered using the BASE smart wallet
+        return smartWallet.isRegistered(msg.sender);
+    }
+
+    // Function to check if a user is registered
+    function isRegistered(address userAddress) public view returns (bool) {
+        // Get the BASE smart wallet instance
+        BaseSmartWallet smartWallet = BaseSmartWallet(address(this));
+
+        // Check if the user is registered using the BASE smart wallet
+        return smartWallet.isRegistered(userAddress);
     }
 }
